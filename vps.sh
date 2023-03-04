eae984b2c32367792a4d390b2b02227f6c0249ec
MJvZ1981 <MJvZanten@gmail.com>
2023-03-04 17:19:56 +0100

    try

--- vps.sh
+++
@@ -1,7 +0,0 @@
-# Let's you enter your vps on the vps command
-function vps() {
-    local ssh_user='root'               # Replace with your SSH username
-    local ssh_host='164.90.194.212'     # Replace with your VPS IP address or hostname
-
-    cd ~ && ssh ${ssh_user}@${ssh_host}
-}
